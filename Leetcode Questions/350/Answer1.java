2b85b7083145b64c20a580abbcc8fda2a2fe996d
Aditya Dagar <adityadagarbro@gmail.com>
2026-09-11 11:11:37 +0530

    Create Answer1.java

---
+++ Leetcode Questions/350/Answer1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums1.length; i++){
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList();
+
+        for(int i = 0; i < nums2.length; i++){
+            int count = map.getOrDefault(nums2[i], 0);
+
+            if (count > 0) {
+            list.add(nums2[i]);
+            map.put(nums2[i], count - 1);
+            }
+        }
+
+        int [] answer = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
