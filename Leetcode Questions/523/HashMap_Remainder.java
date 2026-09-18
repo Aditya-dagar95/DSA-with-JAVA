fb98dd4fb3fef71ae936241916bd8a87b48db0e3
Aditya Dagar <adityadagarbro@gmail.com>
2026-09-18 13:03:36 +0530

    Create HashMap_Remainder.java

---
+++ Leetcode Questions/523/HashMap_Remainder.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int cursum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        for(int i = 0; i < nums.length; i ++){
+            
+            cursum += nums[i];
+            int rem = cursum % k;
+
+            if(!map.containsKey(rem)){
+
+                map.put(rem, i);
+
+            }else if(i - map.get(rem) > 1){
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
